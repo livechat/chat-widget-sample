cdece5a1801abd733132f079b14a3a5868981917
Konrad Kruk <github@konradk.pl>
2017-12-29 15:28:17 +0100

    better index.html layout

--- src/components/App.js
+++ src/components/App.js
@@ -92,29 +92,50 @@ class App extends Component {
         return (
             <ThemeProvider theme={themes[this.state.theme]}>
                 <div style={{
-                    padding: '1em',
-                    maxWidth: '700px',
                 }}>
-                    <h1>
-                        Sample chat widget
-                    </h1>
-                    
-                    <p>Sample chat widget built with <a href="https://docs.livechatinc.com/react-chat-ui-kit/" target="_blank">LiveChat React chat UI kit</a>. In this widget, <a href="https://www.botengine.ai/">BotEngine</a> handles the incoming chats. When the bot returns `LiveChat.transfer` action, the chat is transferred to a human agent together with the transcript of the initial conversation with the bot.</p>
-                    <p>The sample app uses <a href="https://docs.livechatinc.com/visitor-sdk/" target="_blank">Visitor SDK</a> to communicate with LiveChat and <a href="https://docs.botengine.ai/api/introduction">the API</a> to connect with BotEngine.</p>
-                    <p>Source code is avaible at <a href="https://github.com/livechat/chat-widget-sample" target="_blank">Github</a>.</p>
-                    <h3>Change components theme:</h3>
-                    <button id="theme-default" name="default" style={themeDefaultButton} onClick={this.handleThemeChange.bind(this)}>
-                        default
-                    </button>
-                    <button id="theme-purple" name="purple" style={themePurpleButton} onClick={this.handleThemeChange.bind(this)}>
-                        purple
-                    </button>
-                    <button id="theme-dark" name="dark" style={themeDarkButton} onClick={this.handleThemeChange.bind(this)}>
-                        dark
-                    </button>
-                    <button id="theme-elegant" name="elegant" style={themeElegantButton} onClick={this.handleThemeChange.bind(this)}>
-                        elegant
-                    </button>
+                    <header style={{
+                        backgroundColor: '#17212D',
+                        color: '#fff',
+                        padding: '1em',
+                    }}>
+                        <div style={{
+                            maxWidth: '990px',
+                            margin: '0 auto',
+                        }}>
+                            <h1 style={{
+                                fontSize: '3em',
+                                lineHeight: '1em',
+                                fontWeight: '600',
+                            }}>
+                                Chat widget
+                            </h1>
+                            <h2 style={{
+                                color: '#f6e273',
+                                fontWeight: '400',
+                            }}>LiveChat React UI Kit sample</h2>
+                        </div>
+                    </header>
+                    <div style={{
+                        maxWidth: '990px',
+                        margin: '3em auto',
+                    }}>
+                        <p>Sample chat widget built with <a href="https://docs.livechatinc.com/react-chat-ui-kit/" target="_blank">LiveChat React chat UI kit</a>. In this widget, <a href="https://www.botengine.ai/">BotEngine</a> handles the incoming chats. When the bot returns `LiveChat.transfer` action, the chat is transferred to a human agent together with the transcript of the initial conversation with the bot.</p>
+                        <p>The sample app uses <a href="https://docs.livechatinc.com/visitor-sdk/" target="_blank">Visitor SDK</a> to communicate with LiveChat and <a href="https://docs.botengine.ai/api/introduction">the API</a> to connect with BotEngine.</p>
+                        <p>Source code is avaible at <a href="https://github.com/livechat/chat-widget-sample" target="_blank">Github</a>.</p>
+                        <h3>Change components theme:</h3>
+                        <button id="theme-default" name="default" style={themeDefaultButton} onClick={this.handleThemeChange.bind(this)}>
+                            default
+                        </button>
+                        <button id="theme-purple" name="purple" style={themePurpleButton} onClick={this.handleThemeChange.bind(this)}>
+                            purple
+                        </button>
+                        <button id="theme-dark" name="dark" style={themeDarkButton} onClick={this.handleThemeChange.bind(this)}>
+                            dark
+                        </button>
+                        <button id="theme-elegant" name="elegant" style={themeElegantButton} onClick={this.handleThemeChange.bind(this)}>
+                            elegant
+                        </button>
+                    </div>
                     <FixedWrapper.Root maximizedOnInit>
                         <FixedWrapper.Maximized>
                             <Maximized {...this.props} />
